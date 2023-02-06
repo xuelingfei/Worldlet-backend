1d9065fd64bfc827de5b92a7ea10d75aaaf63639
Kylin <xyj.kylin@qq.com>
2023-02-06 17:27:02 +0800

    Update

--- system/models/person.py
+++ system/models/person.py
@@ -13,15 +13,14 @@ class Person(BaseModel):
     ]
 
     name = models.CharField(verbose_name='姓名', null=True, max_length=50)
-    gender = models.CharField(verbose_name='性别', null=True, choices=GENDER_CHOICE)
+    gender = models.IntegerField(verbose_name='性别', null=True, choices=GENDER_CHOICE)
     birthday = models.DateField(verbose_name='生日', null=True)
-    lunar_birthday = models.CharField(verbose_name='农历生日', null=True, max_length=10,
-                                      db_column='lunar_birthday')  # YYYY-RMM-DD 格式，R: 0 非闰月，1 闰月
+    lunar_birthday = models.CharField(verbose_name='农历生日', null=True, max_length=10)  # YYYY-RMM-DD 格式，R: 0 非闰月，1 闰月
     description = models.TextField(verbose_name='描述', null=True)
     signature = models.TextField(verbose_name='个性签名', null=True)
 
     real = models.IntegerField(verbose_name='是否已实名', null=True, choices=BaseModel.IS_OR_NOT, default=BaseModel.NOT)
-    id_number = models.CharField(verbose_name='身份证号', null=True, max_length=50)
+    id_number = models.CharField(verbose_name='身份证号', null=True, max_length=255)
 
     create_user = models.IntegerField(null=True)
     update_user = models.IntegerField(null=True)
